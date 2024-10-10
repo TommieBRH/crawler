1a7713f77b47808ba71da25b6366892aa1a7b089
TommieBRH <tommie.kleijer@broekhuis.nl>
2024-10-10 14:28:47 +0200

    do not run a single url multiple times

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
+file = open("output.txt", "a")
 
-def getUrls(base: str, count: int = 0, urls=[], url_history = {}, stop: int = 1):
+def getUrls(base: str, count: int = 0, urls=[], url_history = {}, stop: int = 1, uri_count: int = 0):
+    soup = None
+    error = False
+
     if count >= stop:
-        return urls
+        return
 
     base_url = f"https://{base}"
+    urls.append(f"{base_url}/")
 
     if count not in url_history:
         url_history[count] = [base_url]
@@ -17,23 +22,36 @@ def getUrls(base: str, count: int = 0, urls=[], url_history = {}, stop: int = 1)
     if new_count not in url_history:
         url_history[new_count] = []
 
+    update_uri_count = uri_count
     for v in url_history[count]:
+        error = False
         resp = requests.get(v)
         soup = BeautifulSoup(resp.text, "html.parser")
+
         for link in soup.find_all("a"):
             uri = link.get("href")
 
             if uri and "/" in uri and uri != "/":
-                if "https" not in uri:
+                if "https" not in uri and "http" not in uri:
                     if not uri.startswith("/"):
                         uri = f"/{uri}"
                     uri = f"{base_url}{uri}"
-                if base in uri and uri not in urls:
+
+                for k, arr in url_history.items():
+                    if uri in arr:
+                        error = True
+                    else:
+                        error = False
+
+                if uri.startswith(base_url) and uri not in urls and not error:
+                    update_uri_count += 1
                     urls.append(uri)
                     url_history[new_count].append(uri)
-                    if "fiets" in uri:
-                        print(uri)
-    getUrls(base=base, count=new_count, urls=urls, url_history=url_history, stop=stop)
+                    print(uri)
+                    file.write(f"{uri} \n")
 
-urls = getUrls(base="broekhuis-hosting.broekhuis.online", stop=5)
-print(urls)
+    getUrls(base=base, count=new_count, urls=urls, url_history=url_history, stop=stop, uri_count=update_uri_count)
+
+
+urls = getUrls(base="broekhuis-hosting.broekhuis.online", stop=10)
+file.close()
